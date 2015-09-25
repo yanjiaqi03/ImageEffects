3e0333c1e16a1a719dd912743cc5a14969b56085
yanjiaqi03 <yanjiaqi@jiuan.com>
2015-09-25 17:32:36 +0800

    文件修改

---
+++ obj/local/armeabi/objs/HelloNDK/HelloNDK.o.d
@@ -0,0 +1,36 @@
+obj/local/armeabi/objs/HelloNDK/HelloNDK.o: jni/HelloNDK.cpp \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/jni.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ jni/com_xiaohuai_ndk_GetString.h jni/com_xiaohuai_ndk_GetInt.h \
+ jni/com_xiaohuai_ndk_ConvertBitmap.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/android/bitmap.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/android/log.h
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/jni.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+jni/com_xiaohuai_ndk_GetString.h:
+
+jni/com_xiaohuai_ndk_GetInt.h:
+
+jni/com_xiaohuai_ndk_ConvertBitmap.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/android/bitmap.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-arm/usr/include/android/log.h:
---
+++ obj/local/x86/objs/HelloNDK/HelloNDK.o.d
@@ -0,0 +1,36 @@
+obj/local/x86/objs/HelloNDK/HelloNDK.o: jni/HelloNDK.cpp \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/jni.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/sys/cdefs.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/sys/cdefs_elf.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/android/api-level.h \
+ jni/com_xiaohuai_ndk_GetString.h jni/com_xiaohuai_ndk_GetInt.h \
+ jni/com_xiaohuai_ndk_ConvertBitmap.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/android/bitmap.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/stdint.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/sys/_types.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/machine/_types.h \
+ /Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/android/log.h
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/jni.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/sys/cdefs.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/android/api-level.h:
+
+jni/com_xiaohuai_ndk_GetString.h:
+
+jni/com_xiaohuai_ndk_GetInt.h:
+
+jni/com_xiaohuai_ndk_ConvertBitmap.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/android/bitmap.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/stdint.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/sys/_types.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/machine/_types.h:
+
+/Users/azlph/Documents/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/android/log.h:
